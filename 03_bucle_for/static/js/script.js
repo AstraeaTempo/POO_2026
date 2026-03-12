031a8ebf3070f93bc7b6f879df7ee2dd8ade93aa
1B0v0 <javierazapata@liceovvh.cl>
2026-03-12 09:00:26 -0300

    a

---
+++ 03_bucle_for/static/js/script.js
@@ -0,0 +1,15 @@
+
+/* 1. El Contador en Pantalla
+Selecciona el párrafo con el id pantalla.
+Crea un ciclo for que vaya del 1 al 10.
+En cada vuelta del ciclo, 
+agrega (concatena con +=) el número actual seguido de un guion 
+(ej: "1 - 2 - 3 - ") al párrafo. */
+
+function contadorPantalla() {
+let numeros = [];
+const container = document.getElementById('resultadoContainer')
+const resultado = document.getElementById('result1');
+
+container.classList.remove('d-none');
+}
