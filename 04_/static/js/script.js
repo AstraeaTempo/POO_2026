d476b7c9b43cfbdbc2d9329b52357962d0515786
1B0v0 <javierazapata@liceovvh.cl>
2026-03-18 15:10:53 -0300

    a

--- 04_/static/js/script.js
+++ 04_/static/js/script.js
@@ -14,9 +14,9 @@ seguido del arreglo. Limpia el input vaciando su .value.
 
 let asistencia = [];
 function agregarLista(nombre) {
-asistencia.push(nombre);
-return asistencia.join(", ");
-}
+    asistencia.push(nombre);
+    return asistencia.join(", ");
+};
 
 function registrarAlumno() {
     // input = ParseInt(input); --> Trasformar a número
@@ -28,7 +28,7 @@ function registrarAlumno() {
     result.textContent = resultado;
     input.value = "";
     container.classList.remove("d-none");
-}
+};
 
 /* Ejercicio 2: Fila de Urgencias Médicas (unshift e if)
 Contexto: En un hospital, los pacientes graves no van al final de la fila, 
@@ -39,9 +39,30 @@ Captura el nombre del paciente desde el input.
 Usa un if para revisar si el input NO está vacío (!== "").
 Si escribieron un nombre, usa .unshift() para agregarlo al inicio del arreglo pacientes.
 Muestra en el textContent del párrafo: "Próximos a atender: " seguido del arreglo.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 3: Sistema de Delivery (shift, pop e if)
+let pacientes = ["Carlos", "María", "Diego"];
+function agregarUrgencia(nombre) {
+    pacientes.unshift(nombre);
+    return pacientes.join(", ");
+};
+
+function ingresarUrgencia() {
+    const container = document.getElementById("container2");
+    const result = document.getElementById("result2");
+    const input = document.getElementById("input2");
+    if (input.value !== "") {
+        let nombre = input.value;
+        let resultado = agregarUrgencia(nombre);
+        result.textContent = resultado;
+        input.value = "";
+        container.classList.remove("d-none");
+    } else {
+        alert("El nombre no puede estar vacio.")
+    };
+};
+
+/* Ejercicio 3: Sistema de Delivery (shift, pop e if)
 Contexto: Un restaurante tiene pedidos listos. 
 El cajero puede despachar el pedido más antiguo, 
 o cancelar el último pedido que entró por un error.
@@ -51,9 +72,11 @@ Captura el texto del input. El usuario debe escribir la palabra "despachar" o "c
 Usa un if. Si escribió "despachar", usa .shift() para sacar la primera comida de la lista.
 Usa un else if. Si escribió "cancelar", usa .pop() para eliminar la última comida de la lista.
 Muestra en el textContent: "Pedidos pendientes: " seguido del arreglo.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 4: Validador de Códigos de Descuento (for e if)
+
+
+/* Ejercicio 4: Validador de Códigos de Descuento (for e if)
 Contexto: Una tienda online revisa si el código promocional 
 que ingresó el cliente existe en su base de datos para aplicarle una rebaja.
 Crea un arreglo: let codigosValidos = ["VERANO2026", "PROMO50", "CLIENTEVIP"];
@@ -64,9 +87,11 @@ Recorre el arreglo con un ciclo for.
 Si el elemento actual del ciclo es igual al texto del input, 
 cambia la variable mensaje a "¡Éxito! Código aceptado".
 Fuera del ciclo, inyecta la variable mensaje en el textContent del párrafo.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 5: Simulador de Cuotas (for)
+
+
+/* Ejercicio 5: Simulador de Cuotas (for)
 Contexto: Un cliente compra un producto y el sistema le genera automáticamente 
 las etiquetas para sus próximas 3 letras de pago.
 Función Principal: Crea simularCuotas().
@@ -76,9 +101,11 @@ Crea un ciclo for que dé exactamente 3 vueltas (del 1 al 3).
 En cada vuelta, súmale (+=) a registroPagos el producto y el número de la cuota 
 (Ej: producto + " - Cuota " + i + " | ").
 Fuera del ciclo, muestra la variable registroPagos en el textContent del párrafo.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 6: Filtro de Presupuesto (for e if)
+
+
+/* Ejercicio 6: Filtro de Presupuesto (for e if)
 Contexto: Una vitrina virtual tiene varios precios. 
 El cliente ingresa cuánta plata tiene en el bolsillo, y el sistema le muestra 
 solo los precios que le alcanza para pagar.
@@ -90,9 +117,11 @@ Recorre el arreglo vitrina con un for.
 Dentro del ciclo, usa un if. Si el precio actual del arreglo es menor o 
 igual (<=) a la plata que ingresó el cliente, súmalo a la variable opciones más un guion (-).
 Muestra el resultado en el textContent del párrafo.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 7: Cálculo de Sueldo Líquido (Una función llama a otra)
+
+
+/* Ejercicio 7: Cálculo de Sueldo Líquido (Una función llama a otra)
 Contexto: El usuario ingresa su Sueldo Bruto. Una función matemática 
 oculta descuenta el 20% (AFP y Salud) y le devuelve el dato a la pantalla para mostrar 
 cuánto dinero real recibirá a fin de mes.
@@ -103,9 +132,11 @@ Captura el sueldo desde el input y conviértelo a número.
 Dentro de procesarSueldo, llama a tu función calcularDescuentos() pasándole el 
 número capturado, y guarda lo que te devuelve en una variable llamada sueldoLiquido.
 Modifica el textContent del párrafo: "Tu sueldo a pago es: $" + sueldoLiquido.
-Limpia el input.
+Limpia el input. */
 
-Ejercicio 8: El Carrito de Compras (Reto Final - Delegación de tareas)
+
+
+/* Ejercicio 8: El Carrito de Compras (Reto Final - Delegación de tareas)
 Contexto: Vamos agregando productos al carrito. Una función anota el producto 
 y le pide ayuda a otra función para que dibuje el carrito actualizado en la pantalla.
 Crea un arreglo vacío: let carrito = [];
@@ -119,3 +150,4 @@ Si el input no está vacío, usa .push() para agregarlo al carrito.
 Al final de esta función, llama a actualizarPantalla() para que la vista se 
 refresque automáticamente con el nuevo producto.
  */
+
